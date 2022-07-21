9e2392bf3a3f12a7ff8f42615705f85dc5a1a75a
407 <luisacano29@hotmail.com>
2022-07-21 17:43:01 -0500

    se modifica el index y se crea controlador

---
+++ src/controllers/controlador.js
@@ -0,0 +1,10 @@
+//CREANDO VARIABLES
+
+let nombreUsuario="Valentina Varela"
+const EDAD_USUARIO=17
+let estaturaUsuario=1.60
+
+console.log(nombreUsuario)
+
+edadUsuario=20
+console.log(edadUsuario)
