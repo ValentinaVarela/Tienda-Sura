3d24555755bc23dd10f22b3c26766e3615eeb6a7
407 <luisacano29@hotmail.com>
2022-07-28 17:36:53 -0500

    se crearon funciones

--- src/controllers/aprendiendoDOM.js
+++ src/controllers/aprendiendoDOM.js
@@ -5,7 +5,19 @@ let etiquetaTitulo=document.getElementById("titulo")
 let etiquetaImagen=document.getElementById("foto")
 
 //1.1 MANIPULANDO EL TEXTO DE LA ETIQUETA (ELEMENTO)
-etiquetaTitulo.textContent="Jefe usted es una gran persona"
+etiquetaTitulo.textContent="La noche tiene la forma de lo que te falta"
 
 //1.2 MANIPULANDO EL SRC
-etiquetaImagen.src="https://mott.pe/noticias/wp-content/uploads/2017/10/Las-fotos-de-paisajes-naturales-m%C3%A1s-hermosos-del-mundo-por-Andi-Campbell_Jones-1280x720.jpg"
+etiquetaImagen.src="https://i.ytimg.com/vi/W3Ia4FLDraQ/maxresdefault.jpg"
+
+//2. CONTROLANDO EL DISEÑO 
+let parrafo=document.getElementById("parrafo")
+parrafo.textContent="“Qué buen insomnio si me desvelo sobre tu cuerpo”"
+
+//2.1 AGREGANDO UN ESTILO (CLASS)
+//text_danger
+parrafo.classList.add("text-danger", "fs-1", "text-center")
+etiquetaImagen.classList.add("d-block", "mx-auto")
+
+//2.2 QUITANDO UN ESTILO
+parrafo.classList.remove("text-danger")
